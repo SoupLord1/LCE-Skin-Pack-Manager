f74febacf9a8d5f900fb957c3c160b75acc1b534
SoupLord1 <113377594+SoupLord1@users.noreply.github.com>
2026-04-19 19:04:03 -0700

    Update edit_packs.py

--- packmanagergui/screens/edit_packs.py
+++ packmanagergui/screens/edit_packs.py
@@ -1,10 +1,13 @@
 import tkinter as tk
+from tkinter import filedialog
 
 from packmanagergui.screens.screen import *
 
 
 class EditPacksScreen(Screen):
+    current_image_path: str = ""
     def __init__(self, root: tk.Tk):
+        self.root = root
         super().__init__(root)
 
         self.screen_type = ScreenType.EDIT_PACKS
@@ -18,7 +21,22 @@ class EditPacksScreen(Screen):
 
         self.pack_name_label.pack()
 
+        add_skin_button = tk.Button(self, text="Add sKin", command= lambda: self.add_skin_popup())
+        add_skin_button.pack()
+
 
     def on_set_screen(self):
         super().on_set_screen()
         self.pack_name_label.config(text=f"Editing '{self.config_manager.current_skinpack.pck_name}'")
+
+    def add_skin_popup(self):
+        popup = tk.Toplevel(self.root)
+        popup.title("Add skin")
+        popup.geometry("250x150")
+        
+        select_image_button = tk.Button(popup, text="Select Skin Image", command=lambda: self.select_skin_image())
+        select_image_button.pack()
+
+    def select_skin_image(self):
+        self.current_image_path = filedialog.askopenfile(filetypes=[("Skin", ".png")])
+        print(self.current_image_path)
